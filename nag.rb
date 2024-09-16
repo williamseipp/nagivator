e22e1ceb6cd890ec4abe67352449e0cf9c5e6c64
William <william.seipp@gmail.com>
2024-09-16 12:27:14 -0700

    typo

--- nag.rb
+++ nag.rb
@@ -1,4 +1,12 @@
-require "sinatra"
+require 'sinatra'
+require 'sinatra/content_for'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+  set :erb, :escape_html => true
+end
 
 get "/" do
   "Welcome to nagivator"
