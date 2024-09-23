27f498c8dd0969ec43b112a9f7a91fded312e033
William <william.seipp@gmail.com>
2024-09-23 15:59:49 -0700

    add logic to open/close db connection after every route is handled

--- nag.rb
+++ nag.rb
@@ -3,12 +3,21 @@ require 'tzinfo'
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
+require_relative 'database_persistence'
+
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
   set :erb, escape_html: true
 end
 
+before do
+  @storage = DatabasePersistence.new(logger)
+end
+
+after do
+  @storage.disconnect
+end
 
 get '/' do
   user_timezone = 'America/Los_Angeles'
