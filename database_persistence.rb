c7404254f1b1c1cc338293195147fdc9cc6fea8f
William <william.seipp@gmail.com>
2024-09-24 11:19:36 -0700

    add ability to fetch all events

--- database_persistence.rb
+++ database_persistence.rb
@@ -18,4 +18,27 @@ class DatabasePersistence
     @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
   end
+
+  def all_events
+    sql = <<~SQL
+      select * from events;
+    SQL
+
+    result = query(sql)
+
+    result.map do |tuple|
+      tuple_to_event_hash(tuple)
+    end
+  end
+
+  private
+
+  def tuple_to_event_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      start_time: tuple["start_time"],
+      end_time: tuple["end_time"],
+      details: tuple["details"],
+      completed: tuple["completed"] }
+  end
 end
